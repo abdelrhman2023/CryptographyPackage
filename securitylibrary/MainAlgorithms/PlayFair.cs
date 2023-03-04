73fc7927050007dec673fcb61f682e55512f5c9e
Nour Kamaly <nourkamaly26@gmail.com>
2023-03-04 14:50:58 +0200

    playfair: implementation of helper functions

--- securitylibrary/MainAlgorithms/PlayFair.cs
+++ securitylibrary/MainAlgorithms/PlayFair.cs
@@ -8,6 +8,8 @@ namespace SecurityLibrary
 {
     public class PlayFair : ICryptographic_Technique<string, string>
     {
+        private char[,] matrix;
+        private List<KeyValuePair<byte, byte>> matrixInverse;
         public string Decrypt(string cipherText, string key)
         {
             throw new NotImplementedException();
@@ -15,21 +17,25 @@ namespace SecurityLibrary
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            string cipherText = "";
+            constructMatrix(ref key, ref matrix, ref matrixInverse);
+            return cipherText.ToUpper();
         }
-        private void constructMatrix(ref string key)
+        private void constructMatrix(ref string key, ref char[,] matrix, ref List<KeyValuePair<byte, byte>> matrixInverse)
         {
-            byte row=0, col=0;
+            byte row = 0, col = 0;
             int index;
-            bool[] alphabet = new bool[26]; 
-            char[,]matrix = new char[5,5];
+            bool[] alphabet = new bool[26];
+            matrix = new char[5, 5];
+            matrixInverse = new List<KeyValuePair<byte, byte>>(25);
             for (int size = 0; size < key.Length; size++)
             {
-                index = (int)key[size]-97;
+                index = (int)key[size] - 97;
                 if (alphabet[index] == false)
                 {
                     alphabet[index] = true;
                     matrix[row, col] = key[size];
+                    matrixInverse[index] = new KeyValuePair<byte, byte>(row, col);
                     if (key[size] == 'i')
                     {
                         alphabet[9] = true;
@@ -43,14 +49,17 @@ namespace SecurityLibrary
                 if (alphabet[index] == false)
                 {
                     matrix[row, col] = (char)(index + 97);
+                    matrixInverse[index] = new KeyValuePair<byte, byte>(row, col);
                     alphabet[index] = true;
+
                     if (index == 8)
                     {
                         alphabet[9] = true;
                     }
                     col++;
                 }
-                reset(ref row,ref col);
+                reset(ref row, ref col);
+
             }
         }
         private void reset(ref byte row, ref byte col)
@@ -60,6 +69,30 @@ namespace SecurityLibrary
                 row += 1;
                 col = 0;
             }
+        }
+        private string getIntersection(ref KeyValuePair<byte, byte> firstLetter, ref KeyValuePair<byte, byte> secondLetter)
+        {
+            string intersection = "";
+            intersection += matrix[firstLetter.Key, secondLetter.Value];
+            intersection += matrix[secondLetter.Key, firstLetter.Value];
+            return intersection;
+        }
+        private char verticalCircularArray(byte x, byte y)
+        {
+            char value = matrix[((x % 4) + 4) % 4, y];
+            return value;
+        }
+        private char horizontalCircularArray(byte x, byte y)
+        {
+            char value = matrix[x, ((y%4)+4)%4];
+            return value;
+        }
+        private string getValues(ref KeyValuePair<byte, byte> firstLetter, ref KeyValuePair<byte, byte> secondLetter, Func<byte, byte, char> circularArray)
+        {
+            string value = "";
+            value += circularArray(firstLetter.Key, firstLetter.Value);
+            value += circularArray(secondLetter.Key, secondLetter.Value);
+            return value;
         }
 
     }
