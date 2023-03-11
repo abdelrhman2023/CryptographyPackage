87b44350685167e5338f88ffbb536342545a4f2f
Nour Kamaly <nourkamaly26@gmail.com>
2023-03-11 21:13:50 +0200

    auto-key and repeating-key vignere encryption test cases done. 4/12

--- securitylibrary/MainAlgorithms/AutokeyVigenere.cs
+++ securitylibrary/MainAlgorithms/AutokeyVigenere.cs
@@ -8,6 +8,7 @@ namespace SecurityLibrary
 {
     public class AutokeyVigenere : ICryptographicTechnique<string, string>
     {
+        char[,] matrix;
         public string Analyse(string plainText, string cipherText)
         {
             throw new NotImplementedException();
@@ -20,7 +21,62 @@ namespace SecurityLibrary
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            constructMatrix(ref matrix);
+            string keyStream = getKeyStream(ref key, ref plainText);
+            string cipherText = null;
+            autoKeyVigenere(keyStream, plainText, ref cipherText);
+            return cipherText.ToUpper();
+        }
+        private void constructMatrix(ref char[,] matrix)
+        {
+            matrix = new char[26, 26];
+            string alphabet = "";
+            int index, innerIndex;
+            char firstLetter;
+            for (index = 0; index < 26; index++)
+            {
+                alphabet += (char)(index + 97);
+            }
+            for (index = 0; index < 26; index++)
+            {
+                for (innerIndex = 0; innerIndex < 26; innerIndex++)
+                {
+                    matrix[index, innerIndex] = alphabet[innerIndex];
+                }
+                firstLetter = alphabet[0];
+                alphabet = alphabet.Remove(0, 1);
+                alphabet = alphabet.Insert(alphabet.Length, firstLetter.ToString());
+            }
+
+        }
+        private string getKeyStream(ref string key, ref string text)
+        {
+            string keyStream = key;
+            int division, remainder;
+            division = text.Length / key.Length;
+            remainder = text.Length % key.Length;
+            for (int repetition = 1; repetition < division; repetition++)
+            {
+                keyStream += text;
+            }
+            for (int r = 0; r < remainder; r++)
+            {
+                keyStream += text[r];
+            }
+            return keyStream;
+        }
+        private void autoKeyVigenere(string keyStream, string text, ref string output)
+        {
+            int size, firstLetter, secondLetter, index;
+            output = "";
+            size = text.Length;
+            for (index = 0; index < size; index++)
+            {
+                firstLetter = (int)(text[index] - 'a');
+                secondLetter = (int)(keyStream[index] - 'a');
+                output += matrix[firstLetter, secondLetter];
+            }
+
         }
     }
 }
--- securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
+++ securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
@@ -8,6 +8,7 @@ namespace SecurityLibrary
 {
     public class RepeatingkeyVigenere : ICryptographicTechnique<string, string>
     {
+        char [,] matrix;
         public string Analyse(string plainText, string cipherText)
         {
             throw new NotImplementedException();
@@ -15,12 +16,71 @@ namespace SecurityLibrary
 
         public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            constructMatrix(ref matrix);
+            string keyStream = getKeyStream(ref key, ref cipherText);
+            string plainText = null;
+            repeatingKeyVigenere(keyStream, cipherText.ToLower(), ref plainText);
+            return plainText;
         }
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            constructMatrix(ref matrix);
+            string keyStream = getKeyStream(ref key,ref plainText);
+            string cipherText = null;
+            repeatingKeyVigenere(keyStream,plainText,ref cipherText);
+            return cipherText.ToUpper();
+        }
+        private void constructMatrix(ref char [,] matrix)
+        {
+            matrix = new char[26,26];
+            string alphabet = "";
+            int index,innerIndex;
+            char firstLetter;
+            for (index = 0; index < 26; index++)
+            {
+                alphabet += (char)(index + 97);
+            }
+            for (index = 0; index < 26; index++)
+            {
+                for (innerIndex = 0;innerIndex < 26; innerIndex++)
+                {
+                    matrix[index,innerIndex] = alphabet[innerIndex];
+                }
+                firstLetter = alphabet[0];
+                alphabet = alphabet.Remove(0, 1);
+                alphabet = alphabet.Insert(alphabet.Length, firstLetter.ToString());
+            }
+            
+        }
+        private string getKeyStream(ref string key,ref string text)
+        {
+            string keyStream = key;
+            int division, remainder;
+            division = text.Length / key.Length;
+            remainder = text.Length % key.Length;
+            for (int repetition = 1; repetition < division; repetition++)
+            {
+                keyStream += key;
+            }
+            for (int r = 0;r < remainder; r++)
+            {
+                keyStream += key[r];
+            }
+            return keyStream;
+        }
+        private void repeatingKeyVigenere(string keyStream, string text, ref string output)
+        {
+            int size, firstLetter, secondLetter,index;
+            output = "";
+            size = text.Length;
+            for (index = 0; index < size; index++)
+            {
+                firstLetter = (int)(text[index] - 'a');
+                secondLetter = (int)(keyStream[index] - 'a');
+                output += matrix[firstLetter, secondLetter];
+            }
+            
         }
     }
 }
