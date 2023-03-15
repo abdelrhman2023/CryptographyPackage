6ac00381fce9e8f34e1b6867d045188e29fcb3c5
Ahmed Magdy <ahmedmagdy2319@gmail.com>
2023-03-15 21:27:34 +0200

    Analysis columner done

--- securitylibrary/MainAlgorithms/Columnar.cs
+++ securitylibrary/MainAlgorithms/Columnar.cs
@@ -10,7 +10,75 @@ namespace SecurityLibrary
     {
         public List<int> Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+
+            bool Flag = false;
+            int coulmn = 0;
+            int row = 0;
+
+            // find number of Columns and Rows
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                if (cipherText[0] == plainText[i])
+                {
+                    //if two letter are similer after eachother in each plain and cipher skip the second and search from next one
+                    // plain  -> attackpostponeduntiltwoam
+                    // cipher -> ttnaaptmtsuoaodwcoiknlpet
+
+                    if (cipherText[1] == plainText[i + 1])
+                    {
+                        i++;
+                    }
+
+                    for (int j = i + 1; j < plainText.Length; j++)
+                        if (cipherText[1] == plainText[j])
+                        {
+                            // number of column = length between the two letters 
+                            coulmn = j - i;
+                            row = plainText.Length / coulmn;
+                            Flag = true;
+                            break;
+                        }
+                }
+                if (Flag == true)
+                {
+                    break;
+                }
+            }
+
+            //Console.WriteLine(coulmn);
+
+            // create matrix of plaintext
+            int count = 0;
+            int[,] matrix = new int[row, coulmn];
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < coulmn; j++)
+                {
+                    matrix[i, j] = plainText[count];
+                    count++;
+                }
+            }
+
+            //find key of cipher
+            int colNum = 1;
+            int[] arrKey = new int[coulmn];
+            for (int i = 0; i < cipherText.Length - 1; i += row)
+            {
+                for (int j = 0; j < coulmn; j++)
+                {
+                    if (cipherText[i] == matrix[0, j] && cipherText[i + 1] == matrix[1, j])
+                    {
+                        arrKey[j] = colNum;
+                        colNum++;
+                        break;
+                    }
+                }
+
+            }
+            List<int> key = arrKey.ToList<int>();
+
+            return key;
         }
 
         public string Decrypt(string cipherText, List<int> key)
