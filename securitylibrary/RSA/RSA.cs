bc9a2e4df7d2827781f7432ffdf5e89555f6566b
AhmeeDSaMyy <103587594+AhmeeDSaMyy@users.noreply.github.com>
2023-04-26 23:20:20 +0200

    all test cases done

--- securitylibrary/RSA/RSA.cs
+++ securitylibrary/RSA/RSA.cs
@@ -8,14 +8,31 @@ namespace SecurityLibrary.RSA
 {
     public class RSA
     {
+        AES.ExtendedEuclid inverse = new AES.ExtendedEuclid();
+        DiffieHellman.DiffieHellman DH = new DiffieHellman.DiffieHellman();
         public int Encrypt(int p, int q, int M, int e)
         {
-            throw new NotImplementedException();
+            int result = 1;
+            int n = p * q;
+            
+            DH.fastPower(M,e,n); 
+
+            return result;
+
         }
 
         public int Decrypt(int p, int q, int C, int e)
         {
-            throw new NotImplementedException();
+            int  result =1;
+            int  n = p * q;
+            int Qn =(p-1)*(q-1);
+
+            int d = inverse.GetMultiplicativeInverse(e, Qn);
+
+            DH.fastPower(C, d, n);
+
+
+            return result;
         }
     }
 }
